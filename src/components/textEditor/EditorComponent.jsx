08607382c243330d619f420c7627fa1798a8f4e5
gmako6 <37672372+gmako6@users.noreply.github.com>
2024-12-10 13:49:42 +0300

    Update EditorComponent.jsx

--- src/components/textEditor/EditorComponent.jsx
+++ src/components/textEditor/EditorComponent.jsx
@@ -10,10 +10,7 @@ import { HistoryPlugin } from "@lexical/react/LexicalHistoryPlugin";
 import { LexicalErrorBoundary } from "@lexical/react/LexicalErrorBoundary";
 import { useLexicalComposerContext } from "@lexical/react/LexicalComposerContext";
 
-const theme = {
-  // Theme styling goes here
-  //...
-};
+
 
 // Catch any errors that occur during Lexical updates and log them
 function onError(error) {
@@ -33,7 +30,7 @@ function MyOnChangePlugin({ onChange }) {
 export default function EditorComponent() {
   const initialConfig = {
     namespace: "MyEditor",
-    theme,
+    theme: exampleTheme,
     onError,
   };
 
@@ -65,3 +62,74 @@ export default function EditorComponent() {
     </LexicalComposer>
   );
 }
+
+const exampleTheme = {
+  ltr: "ltr",
+  rtl: "rtl",
+  paragraph: "",
+  quote: "editor-quote",
+  heading: {
+    h1: "text-3xl font-bold",
+    h2: "editor-heading-h2",
+    h3: "editor-heading-h3",
+    h4: "editor-heading-h4",
+    h5: "editor-heading-h5",
+    h6: "editor-heading-h6",
+  },
+  list: {
+    nested: {
+      listitem: "editor-nested-listitem",
+    },
+    ol: "editor-list-ol",
+    ul: "editor-list-ul",
+    listitem: "editor-listItem",
+    listitemChecked: "editor-listItemChecked",
+    listitemUnchecked: "editor-listItemUnchecked",
+  },
+  hashtag: "editor-hashtag",
+  image: "editor-image",
+  link: "editor-link",
+  text: {
+    bold: "font-bold",
+    code: "editor-textCode",
+    italic: "italic",
+    strikethrough: "editor-textStrikethrough",
+    subscript: "editor-textSubscript",
+    superscript: "editor-textSuperscript",
+    underline: "editor-textUnderline",
+    underlineStrikethrough: "editor-textUnderlineStrikethrough",
+  },
+  code: "editor-code",
+  codeHighlight: {
+    atrule: "editor-tokenAttr",
+    attr: "editor-tokenAttr",
+    boolean: "editor-tokenProperty",
+    builtin: "editor-tokenSelector",
+    cdata: "editor-tokenComment",
+    char: "editor-tokenSelector",
+    class: "editor-tokenFunction",
+    "class-name": "editor-tokenFunction",
+    comment: "editor-tokenComment",
+    constant: "editor-tokenProperty",
+    deleted: "editor-tokenProperty",
+    doctype: "editor-tokenComment",
+    entity: "editor-tokenOperator",
+    function: "editor-tokenFunction",
+    important: "editor-tokenVariable",
+    inserted: "editor-tokenSelector",
+    keyword: "editor-tokenAttr",
+    namespace: "editor-tokenVariable",
+    number: "editor-tokenProperty",
+    operator: "editor-tokenOperator",
+    prolog: "editor-tokenComment",
+    property: "editor-tokenProperty",
+    punctuation: "editor-tokenPunctuation",
+    regex: "editor-tokenVariable",
+    selector: "editor-tokenSelector",
+    string: "editor-tokenSelector",
+    symbol: "editor-tokenProperty",
+    tag: "editor-tokenProperty",
+    url: "editor-tokenOperator",
+    variable: "editor-tokenVariable",
+  },
+};
