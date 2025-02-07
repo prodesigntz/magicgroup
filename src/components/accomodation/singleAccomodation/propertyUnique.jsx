4c65d4f29de6a81d51a39591ac79031e55fa950d
Pro DesignTZ <prodesigntz@gmail.com>
2025-02-07 14:45:29 +0300

    corrected edit company

--- src/components/accomodation/singleAccomodation/propertyUnique.jsx
+++ src/components/accomodation/singleAccomodation/propertyUnique.jsx
@@ -73,7 +73,7 @@ export default function PropertyUnique({property}) {
             />
 
            
-          ))}</>:<h1>No <Services></Services></h1>}
+          ))}</>:<h1>No Services</h1>}
         </div>
       </div>
     </section>
