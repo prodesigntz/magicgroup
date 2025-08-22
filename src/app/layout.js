b67c7c737d72fd183c2c745cb7f6cf49ce6e6c8a
Pro DesignTZ <prodesigntz@gmail.com>
2025-08-22 18:01:37 +0300

    verification

--- src/app/layout.js
+++ src/app/layout.js
@@ -7,6 +7,8 @@ import {
 } from "next/font/google";
 import "./globals.css";
 import { AppContextProvider } from "@/context/AppContext";
+import Head from 'next/head';
+
 
 
 
@@ -41,6 +43,9 @@ export const metadata = {
   title: "Magic Group",
   description: 
     "Where Nature Meets Luxury, Best Luxurious Safari accomodations in Tanzania",
+     other: {
+    'simpledcver': 'eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJkb21haW4iOiJtYWdpY2dyb3VwLmNvLnR6IiwiZXhwIjoxNzU2NDI1NjAwfQ.Y5tkV0l3wBfPAhPC5v2EEABJIKfEWS75_c8R5d5baXQ',
+  },
 };
 
 export default function RootLayout({ children }) {
@@ -48,6 +53,8 @@ export default function RootLayout({ children }) {
     <html lang="en">
       <head>
         <meta charSet="UTF-8" />
+             <meta name="simpledcver" content="eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJkb21haW4iOiJtYWdpY2dyb3VwLmNvLnR6IiwiZXhwIjoxNzU2NDI1NjAwfQ.Y5tkV0l3wBfPAhPC5v2EEABJIKfEWS75_c8R5d5baXQ" />
+     
       </head>
 
       <body
